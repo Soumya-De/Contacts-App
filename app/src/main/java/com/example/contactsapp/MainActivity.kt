a494f2ac1a358ce149091155f450d9fd91bfe542
Soumya-De <soumyadey820@gmail.com>
2024-08-23 03:42:26 +0530

    added logic for recycler view

--- app/src/main/java/com/example/contactsapp/MainActivity.kt
+++ app/src/main/java/com/example/contactsapp/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,40 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val listOfContact = mutableListOf<ContactItem>()
+        val rv = findViewById<RecyclerView>(R.id.rv)
+        listOfContact.add(ContactItem(
+            name = "Soumya",
+            phoneNumber = "1234567890",
+            imageRes = R.drawable.user
+        ))
+        listOfContact.add(ContactItem(
+            name = "Soumya",
+            phoneNumber = "1234567890",
+            imageRes = R.drawable.man
+        ))
+        listOfContact.add(ContactItem(
+            name = "Soumya",
+            phoneNumber = "1234567890",
+            imageRes = R.drawable.woman
+        ))
+        listOfContact.add(ContactItem(
+            name = "Soumya",
+            phoneNumber = "1234567890",
+            imageRes = R.drawable.student
+        ))
+        listOfContact.add(ContactItem(
+            name = "Soumya",
+            phoneNumber = "1234567890",
+            imageRes = R.drawable.teacher
+        ))
+        listOfContact.add(ContactItem(
+            name = "Soumya",
+            phoneNumber = "1234567890",
+            imageRes = R.drawable.doctor
+        ))
+        val adapter = ContactListAdapter(listOfContact)
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = adapter
     }
 }
