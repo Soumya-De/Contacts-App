d38e08b3574a1941675a39f1b0550d43a02ab4e8
Soumya-De <soumyadey820@gmail.com>
2024-08-23 03:43:41 +0530

    adapter created for the rv to represent view to users

---
+++ app/src/main/java/com/example/contactsapp/ContactListAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.contactsapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactListAdapter(
+    private val listOfContact: List<ContactItem>
+):RecyclerView.Adapter<ContactListAdapter.ContactListViewHolder>() {
+    class ContactListViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val name = view.findViewById<TextView>(R.id.name)
+        val phoneNumber = view.findViewById<TextView>(R.id.phoneno)
+        val imageRes = view.findViewById<ImageView>(R.id.image)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactListViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_rv_item,parent,false)
+        return ContactListViewHolder(view)
+
+    }
+
+    override fun getItemCount(): Int {
+        return listOfContact.size
+    }
+
+    override fun onBindViewHolder(holder: ContactListViewHolder, position: Int) {
+        holder.name.text = listOfContact[position].name
+        holder.phoneNumber.text = listOfContact[position].phoneNumber
+        holder.imageRes.setImageResource(listOfContact[position].imageRes)
+    }
+
+}
